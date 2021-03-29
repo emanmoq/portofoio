bd297c1aef6a19e166f0596361070e0bcf473a66
emanmoq <42995029+emanmoq@users.noreply.github.com>
2021-03-29 12:12:00 -0700

    upload

---
+++ assets/js/main.js
@@ -0,0 +1,59 @@
+jQuery(function ($) {
+
+  function onScroll(event){
+			
+		var scrollPosition = $(document).scrollTop();
+    //add class active when select item from navbar 
+		$('.navbar-collapse .nav-item a[href^="#"').each(function () {
+			console.log($(this).attr('href')); 
+		  var refElement = $($(this).attr("href"));
+		  if (refElement.length&&refElement.position().top <= scrollPosition) {
+			$('.navbar-collapse .nav-item ').removeClass("active");
+			$(this).parent().addClass("active");
+		  }
+		  else{
+			$(this).removeClass("active");
+		  }
+  
+		});
+	  }
+    $(document).on("scroll", onScroll);
+// animate to the section when click  on it
+    $('.navbar-collapse ul li a').on('click', function () {
+      $('html, body').animate({ scrollTop: $(this.hash).offset().top  - 5 }, 1000);
+      $('.navbar-collapse.collapse.in').removeClass('in');
+      return false;
+    });
+    
+  $(window).scroll(function () {
+    scrollTop = $(window).scrollTop();
+//add fix header when scroll down and remove when back up
+    if (scrollTop > $('header').height()) {
+      $('header').addClass('scrollNav');
+    }
+
+    else {
+      $('header').removeClass('scrollNav');
+    }
+
+  });
+  //close menu when choose item
+  $(".navbar .nav-link").click(function(){
+    $(".collapse").removeClass("show");
+  });
+  $(".navbar-toggler").click(function(){
+    $(".overlay").css("display","block")
+  });
+  $(".close").click(function(){
+    $(".overlay").css("display","none");
+    $(".collapse").removeClass('show');
+  })
+  $(".overlay").click(function(){
+    $(this).css("display","none");
+    $(".collapse").removeClass('show');
+  });
+//progressbar loading
+$(".progress-bar").loading();
+
+});
+
